bfb916f8b2b4e4b8cb2c8b7b7ec1fa37519d1bec
Yoshiki Wada <yoshiki.wada@gmail.com>
2014-07-07 09:08:58 +0900

    Calculate differential coefficient instead of explicit expression

--- a.ml
+++ a.ml
@@ -4,57 +4,63 @@ open ExtLib
 let maxiter = 50
 let epsabs = 1e-4
 let epsrel = 1e-4
-let p = 3
+let num_x = 3
 
-let expb t y sigma =
-  let expb_f ~x ~f =
-    assert (Gsl.Vector.length x = p);
+let my_fun t x =
+  assert (Gsl.Vector.length x = num_x);
+  let open! Flop in
+  x.{0} * exp (-((t - x.{1})**2. / (2. * x.{2}**2.)))
+
+let df_my_fun t x n =
+  assert (Gsl.Vector.length x = num_x);
+  let xa = Gsl.Vector.copy x in
+  let xb = Gsl.Vector.copy x in
+  xa.{n} <- x.{n} -. epsabs;
+  xb.{n} <- x.{n} +. epsabs;
+  let ya = my_fun t xa in
+  let yb = my_fun t xb in
+  (yb -. ya) /. (2. *. epsabs)
+
+let multi_funs t y sigma =
+  let multi_f ~x ~f =
+    assert (Gsl.Vector.length x = num_x);
     let n = Gsl.Vector.length f in
     assert (Array.length t = n);
     assert (Array.length y = n);
     assert (Array.length sigma = n);
-    let na = x.{0} in
-    let m = x.{1} in
-    let s = x.{2} in
     for i = 0 to pred n do
       (* model Yi = na * exp(-(x-m)**2 / (2*s**2)) *)
       let open! Flop in
-      let ex = exp (-((t.(i)-m)**2. / (2.*s**2.))) in
-      let yi = na * ex in
+      let yi = my_fun t.(i) x in
       f.{i} <- (yi - y.(i)) / sigma.(i)
     done
   in
-  let expb_df ~x ~j =
+  let multi_df ~x ~j =
     let (n, p) = Gsl.Matrix.dims j in
     assert (Gsl.Vector.length x = p);
     assert (Array.length sigma = n);
-    let na = x.{0} in
-    let m = x.{1} in
-    let s = x.{2} in
     for i = 0 to pred n do
       (* Jacobian matrix J(i,j) = dfi / dxj,        *) 
       (* where fi = (Yi - yi)/sigma[i],             *) 
       (*       Yi = na * exp(-(x-m)**2 / (2*s**2)) *)
       (* and the xj are the parameters (A,lambda,b) *) 
       let sgm = sigma.(i) in
-      let open! Flop in
-      let ex = exp (-((t.(i)-m)**2. / (2.*s**2.))) in
-      j.{i, 0} <- ex / sgm;
-      j.{i, 1} <- na * (t.(i)-m)     * ex / s**2. / sgm;
-      j.{i, 2} <- na * (t.(i)-m)**2. * ex / s**3. / sgm
+      for k = 0 to num_x - 1 do
+        j.{i, k} <- df_my_fun t.(i) x k /. sgm
+      done
     done
   in
-  let expb_fdf ~x ~f ~j =
-    expb_f ~x ~f ;
-    expb_df ~x ~j
+  let multi_fdf ~x ~f ~j =
+    multi_f ~x ~f;
+    multi_df ~x ~j
   in
-  { Gsl.Fun.multi_f = expb_f ;
-    Gsl.Fun.multi_df = expb_df ;
-    Gsl.Fun.multi_fdf = expb_fdf ;
+  { Gsl.Fun.multi_f = multi_f;
+    Gsl.Fun.multi_df = multi_df;
+    Gsl.Fun.multi_fdf = multi_fdf;
   } 
 
 let print_pos fit =
-  let pos = Gsl.Vector.create p in
+  let pos = Gsl.Vector.create num_x in
   Gsl.Multifit_nlin.position fit pos;
   printf "%g\t%g\t%g\n" pos.{0} pos.{1} pos.{2}
 
@@ -102,6 +108,6 @@ let () =
   assert (n = Array.length y);
   let sigma = Array.make n 0.1 in
   let xinit = Gsl.Vector.of_array [| 1.0; 0.1; 1.0 |] in
-  let fit = Gsl.Multifit_nlin.make Gsl.Multifit_nlin.LMSDER ~n ~p (expb t y sigma) xinit in
+  let fit = Gsl.Multifit_nlin.make Gsl.Multifit_nlin.LMSDER ~n ~p:num_x (multi_funs t y sigma) xinit in
   solve fit;
   print_pos fit
